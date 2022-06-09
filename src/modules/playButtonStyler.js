cd8890d6fb8e3a4658b46dc44537df1bbc20f12f
TonyFef <fefilov.toha1999@yandex.ru>
2022-06-09 10:42:45 +0300

    fixed images

--- src/modules/playButtonStyler.js
+++ src/modules/playButtonStyler.js
@@ -4,7 +4,7 @@ const playButtonStyler = () => {
     playBtn.style.position = "relative";
     playBtn.style.backgroundColor = "transparent";
     playBtn.innerHTML =
-        '<img class="video-play__button" src="./images/vector1.svg" alt="/"><img class="video-play__button" src="./images/vector2.svg" alt="/"><img class="video-play__button" src="./images/vector3.svg" alt="/"><img class="video-play__button" src="./images/vector4.svg" alt="/">';
+        '<img class="video-play__button" src="./images/playButtonVector1.svg" alt="/"><img class="video-play__button" src="./images/playButtonVector2.svg" alt="/"><img class="video-play__button" src="./images/playButtonVector3.svg" alt="/"><img class="video-play__button" src="./images/playButtonVector4.svg" alt="/">';
 };
 
 export default playButtonStyler;
