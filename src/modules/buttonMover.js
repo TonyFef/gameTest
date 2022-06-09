d0e7b00390c9de0b72ab15757a752723241eb92b
TonyFef <fefilov.toha1999@yandex.ru>
2022-06-09 10:39:21 +0300

    add mediaQuery

--- src/modules/buttonMover.js
+++ src/modules/buttonMover.js
@@ -2,9 +2,25 @@ import { animate, getRandom } from "../modules/helpers";
 
 export const buttonMover = (duration) => {
     const button = document.querySelector(".vjs-big-play-button");
+    let windowWidthUsable;
+    let windowHeightUsable;
 
-    const windowWidthUsable = window.outerWidth * 0.6;
-    const windowHeightUsable = window.outerHeight * 0.8;
+    const windowWidth = window.outerWidth;
+    console.log(windowWidth);
+
+    if (windowWidth > 1400) {
+        windowWidthUsable = window.outerWidth * 0.6;
+        windowHeightUsable = window.outerHeight * 0.8;
+    } else if (windowWidth > 800 && windowWidth <= 1400) {
+        windowWidthUsable = window.outerWidth * 0.4;
+        windowHeightUsable = window.outerHeight * 0.7;
+    } else if (windowWidth > 500 && windowWidth <= 800) {
+        windowWidthUsable = window.outerWidth * 0.4;
+        windowHeightUsable = window.outerHeight * 0.5;
+    } else if (windowWidth <= 500) {
+        windowWidthUsable = window.outerWidth * 0.55;
+        windowHeightUsable = window.outerHeight * 0.5;
+    }
 
     let buttonMovingInterval = setInterval(() => {
         animate({
